c90828f310051adcd3ff748002d9bcafcad95f27
Uma Khasanova <106467856+u-khasanova@users.noreply.github.com>
2025-04-28 12:48:32 +0300

    Ускорен поиск нужных строк

--- src/DOEventExtractor.scala
+++ src/DOEventExtractor.scala
@@ -3,18 +3,10 @@ import org.apache.spark.rdd.RDD
 
 object DOEventExtractor {
   def extractEvents(inputRDD: RDD[String]): RDD[String] = {
-    // find QS-lines indices
-    val doIndices = inputRDD.zipWithIndex()
-      .filter { case (line, _) => line.startsWith("DOC_OPEN") }
-      .map { case (_, idx) => idx }
-      .collect()
-      .toSet
-
-    // return DOC_OPEN lines
-    inputRDD.zipWithIndex()
-      .filter { case (_, idx) => doIndices.contains(idx)}
-      .map { case (line, idx) => (idx, line) }
-      .groupByKey()
-      .map { case (_, lines) => lines.mkString(" ") }
+    inputRDD.mapPartitions { iter =>
+      iter.sliding(1).collect {
+        case Seq(doLine) if doLine.startsWith("DOC_OPEN") => s"$doLine"
+      }
+    }
   }
 }
--- src/QSEventExtractor.scala
+++ src/QSEventExtractor.scala
@@ -3,18 +3,10 @@ import org.apache.spark.rdd.RDD
 
 object QSEventExtractor {
   def extractEvents(inputRDD: RDD[String]): RDD[String] = {
-    // find QS-lines indices
-    val qsIndices = inputRDD.zipWithIndex()
-      .filter { case (line, _) => line.startsWith("QS") }
-      .map { case (_, idx) => idx }
-      .collect()
-      .toSet
-
-    // join QS-lines with next line
-    inputRDD.zipWithIndex()
-      .filter { case (_, idx) => qsIndices.contains(idx) || qsIndices.contains(idx - 1) }
-      .map { case (line, idx) => (if (qsIndices.contains(idx)) idx else idx - 1, line) }
-      .groupByKey()
-      .map { case (_, lines) => lines.mkString(" ") }
+    inputRDD.mapPartitions { iter =>
+      iter.sliding(2).collect {
+        case Seq(qsLine, nextLine) if qsLine.startsWith("QS") => s"$qsLine $nextLine"
+      }
+    }
   }
 }
