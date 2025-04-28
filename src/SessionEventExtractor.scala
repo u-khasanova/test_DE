079f7c7c8411445400e458931a3e657963591f01
Uma Khasanova <106467856+u-khasanova@users.noreply.github.com>
2025-04-28 12:48:53 +0300

    Все еще не работает корректно

--- src/SessionEventExtractor.scala
+++ src/SessionEventExtractor.scala
@@ -1,40 +1,23 @@
 package org.example
 import org.apache.spark.rdd.RDD
+import scala.collection.mutable
 
 object SessionEventExtractor {
   def extractEvents(inputRDD: RDD[String]): RDD[String] = {
-    // 1. Собираем индексы всех стартовых событий
-    val startIndices = inputRDD.zipWithIndex()
-      .filter { case (line, _) => line.startsWith("SESSION_START") }
-      .map { case (_, idx) => idx }
-      .collect()
-      .toSet
+    inputRDD.mapPartitions { iter =>
+      val buffer = new mutable.Queue[String]()
+      val result = new mutable.ListBuffer[String]()
 
-    // 2. Собираем индексы всех конечных событий
-    val endIndices = inputRDD.zipWithIndex()
-      .filter { case (line, _) => line.startsWith("SESSION_END") }
-      .map { case (_, idx) => idx }
-      .collect()
-      .toSet
+      for (line <- iter) {
+        if (line.startsWith("SESSION_START")) {
+          buffer.enqueue(line)
+        } else if (line.startsWith("SESSION_END") && buffer.nonEmpty) {
+          val start = buffer.dequeue()
+          result += s"$start $line" // объединяем пару
+        }
+      }
 
-    // 3. Сопоставляем каждому START ближайший следующий END
-    val sessionPairs = startIndices.flatMap { startIdx =>
-      endIndices.find(endIdx => endIdx > startIdx).map(endIdx => (startIdx, endIdx))
+      result.iterator
     }
-
-    // 4. Извлекаем полные пары
-    inputRDD.zipWithIndex()
-      .filter { case (_, idx) =>
-        sessionPairs.exists(p => p._1 == idx || p._2 == idx)
-      }
-      .map { case (line, idx) =>
-        sessionPairs.find(p => p._1 == idx || p._2 == idx).get -> line
-      }
-      .groupByKey()
-      .map { case ((startIdx, endIdx), lines) =>
-        val sortedLines = lines.toSeq.sortBy(line =>
-          if (line.startsWith("SESSION_START")) 0 else 1)
-        sortedLines.mkString(" ")
-      }
   }
 }
--- src/SessionEventParser.scala
+++ src/SessionEventParser.scala
@@ -6,13 +6,12 @@ import scala.util.matching.Regex
 
 object SessionEventParser {
   case class ParsedSessionRecord(dateStart: DateTimeParts, dateEnd: DateTimeParts)
-
   private val sessionPattern: Regex =
     """^SESSION_START\s+([^\s]+).*?SESSION_END\s+([^\s]+).*?""".r
 
   def parseSessionLine(line: String): Option[ParsedSessionRecord] = {
     line match {
-      case sessionPattern(startDateStr, endDateStr) =>  // Исправлено: разные имена для групп
+      case sessionPattern(startDateStr, endDateStr) =>
         for {
           start <- DateTimeParser.parse(startDateStr)
           end <- DateTimeParser.parse(endDateStr)
