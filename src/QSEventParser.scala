cee65e00666cb6d662d41b84e28cbba3fa49f106
Uma Khasanova <106467856+u-khasanova@users.noreply.github.com>
2025-04-28 13:34:35 +0300

    Добавлен объект QSEventParserTest для тестов

--- src/QSEventParser.scala
+++ src/QSEventParser.scala
@@ -1,6 +1,7 @@
 package org.example
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-
+import org.example.QSEventParser.parseQSRecords
 import scala.util.matching.Regex
 
 object QSEventParser {
@@ -12,8 +13,9 @@ object QSEventParser {
     line match {
       case qsPattern(dateStr, id, codesStr) =>
         DateTimeParser.parse(dateStr).map { dtParts =>
+          val cleanedId = if (id.startsWith("-")) id.substring(1) else id
           ParsedQSRecord(
-            id,
+            cleanedId,  // Используем очищенный ID без минуса
             dtParts,
             if (codesStr.trim.isEmpty) Nil else codesStr.trim.split("\\s+").toList
           )
@@ -26,10 +28,43 @@ object QSEventParser {
   }
 }
 
-//val testData = Seq(
-//      "QS 19.08.2020_01:51:34 {329-фз} 13693734 LAW_140194 LAW_304061 LAW_189522 LAW_359169 LAW_334459 LAW_170478 LAW_201517",
-//      "QS 19.08.2020_01:51:34 {329-фз} -13693734 LAW_140194 LAW_304061 LAW_189522 LAW_359169 LAW_334459 LAW_170478 LAW_201517",
-//      "QS Fri,_19_Aug_2020_01:51:34_+0300 {329-фз} -13693734 LAW_140194 LAW_304061 LAW_189522 LAW_359169 LAW_334459 LAW_170478 LAW_201517",
-//      "QS  {329-фз} 13693734 LAW_140194 LAW_304061 LAW_189522 LAW_359169 LAW_334459 LAW_170478 LAW_201517",
-//      "QS 19.08.2020_01:51:34 {329-фз} 13693734 "
-//)
+object QSEventParserTest extends App {
+  val testData = Seq(
+    "QS 19.08.2020_01:51:34 {329-фз} 13693734 LAW_140194 LAW_304061",
+    "QS 19.08.2020_01:51:34 {329-фз} -13693734 LAW_140194 LAW_304061",
+    "QS Fri,_19_Aug_2020_01:51:34_+0300 {329-фз} -13693734 LAW_140194 LAW_304061",
+    "QS  {329-фз} 13693734 LAW_140194 LAW_304061",
+    "QS 19.08.2020_01:51:34 {329-фз} 13693734 ",
+    "INVALID DATA"
+  )
+
+  val sc = new SparkContext(new SparkConf().setMaster("local").setAppName("test"))
+  try {
+    println("="*50)
+    println("Starting QSEventParser tests")
+    println("="*50 + "\n")
+
+    val rdd = sc.parallelize(testData)
+    val results = parseQSRecords(rdd).collect()
+
+    testData.zipWithIndex.foreach { case (record, idx) =>
+      println(s"Record #${idx + 1}: ${record}")
+      println("-"*40)
+    }
+
+    println(s"Succesfully parsed records: ${results.length}\n")
+
+    results.zipWithIndex.foreach { case (record, idx) =>
+      println(s"Record #${idx + 1}:")
+      println(s"ID: ${record.id}")
+      println(s"Date: ${record.date.date} ${record.date.time}")
+      println(s"Codes: ${record.codes.mkString(", ")}")
+      println("-"*40)
+    }
+
+    val invalidCount = testData.size - results.length
+    println(s"\nParsing is failed $invalidCount times")
+  } finally {
+    sc.stop()
+  }
+}
