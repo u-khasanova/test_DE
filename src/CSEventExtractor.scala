4e3e58ed839812b305c1b9f0e56d9785dd202e4f
Uma Khasanova <106467856+u-khasanova@users.noreply.github.com>
2025-04-28 12:50:08 +0300

    commit

--- src/CSEventExtractor.scala
+++ src/CSEventExtractor.scala
@@ -3,10 +3,8 @@ import org.apache.spark.rdd.RDD
 
 object CSEventExtractor {
   def extractEvents(inputRDD: RDD[String]): RDD[String] = {
-    // Собираем все строки с индексами
     val linesWithIndex = inputRDD.zipWithIndex().collect()
 
-    // Находим пары (startIndex, endIndex) для каждого события
     val eventRanges = {
       var currentStart: Option[Long] = None
       linesWithIndex.flatMap { case (line, idx) =>
@@ -23,21 +21,16 @@ object CSEventExtractor {
       }
     }
 
-    // Собираем события в нужном формате
     val events = eventRanges.map { case (startIdx, endIdx) =>
-      // Получаем все строки события (от START до END включительно)
       val eventLines = (startIdx to endIdx + 1).map(i =>
         linesWithIndex.find(_._2 == i).map(_._1).getOrElse(""))
 
-      // Разбираем компоненты
       val startLine = eventLines.head
-      val queryLines = eventLines.tail.init // Все строки между START и END
+      val queryLines = eventLines.tail.init
       val endLine = eventLines.last
 
-      // Извлекаем результаты из строки после CARD_SEARCH_END
       val searchResults = endLine.replace("CARD_SEARCH_END", "").trim
 
-      // Формируем итоговую строку
       s"$startLine ${queryLines.mkString(" ")} $searchResults".replaceAll("\\s+", " ").trim
     }
 
